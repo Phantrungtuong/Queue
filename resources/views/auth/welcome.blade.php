3e86614fdeadce72f55ececb3dad57a8d05a45b0
phantrungtuong <phantrungtuong95@gmail.com>
2019-06-19 15:29:43 +0700

    Queue

---
+++ resources/views/auth/welcome.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Laravel</title>
+</head>
+<body>
+    <h1>Chúc mừng bạn đã đăng ký thành công</h1>
+</body>
+</html>
---
+++ resources/views/mail/welcome.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Welcome Laravel</title>
+</head>
+<body>
+<h1>Chào mừng đến với Laravel queue.lar.vn</h1>
+</body>
+</html>
